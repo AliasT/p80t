67fa40582ca85c864b77bb7dfe3aff0f479ebeb9
chaixb <1481569358@qq.com>
2019-03-27 16:48:37 +0800

    updates

--- main.go
+++ main.go
@@ -6,74 +6,75 @@ import (
 	"github.com/cbednarski/hostess"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
-	"net"
+	"net/http"
 	"os"
 )
 
+// Server 配置文件的结构表示
 type Server struct {
-	host []byte
-	port int
+	name string
+	host string
+	port float64
 }
 
 func main() {
-	hostsfile, errs := hostess.LoadHostfile()
-	if len(errs) > 0 {
+	hostsfile, _ := hostess.LoadHostfile()
+	// hostname := hostess.Hostname{"localhost", net.IPv4(127, 0, 0, 1), true, false}
 
-	}
+	servers, _ := ReadJSONConfig()
 
-	hostname := hostess.Hostname{"localhost", net.IPv4(127, 0, 0, 1), true, false}
-
-	servers, err := ReadJSONConfig()
-	if err != nil {
-
-	}
-
-	for name, s := range servers {
-		server := s.(map[string]interface{})
-		fmt.Println(server["host"], server["port"], name)
-
-		if hostsfile.Hosts.Contains(&hostname) {
-			fmt.Println("exists a server refer to local")
-		}
-	}
-
-	Serve()
+	Serve(hostsfile, servers)
 }
 
-func Serve() {
+// Serve 本地运行的gin服务器
+func Serve(hostfile *hostess.Hostfile, servers *[]Server) {
 	r := gin.Default()
-	r.Use(Transfer())
+	r.Use(Transfer(hostfile, servers))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// listen and serve on 0.0.0.0:8080
+	r.Run(":80")
 }
 
-// middleware
-func Transfer() gin.HandlerFunc {
+// Transfer 将不同域名的请求转发至配置文件的指定端口
+func Transfer(hostfile *hostess.Hostfile, servers *[]Server) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Write([]byte("hello"))
+		target := "https://baidu.com"
+		if c.Request.Host == "gui.xiaoma.cn" {
+			target = "https://sohu.com"
+		}
+		res, _ := http.Get(target)
+		bytes, _ := ioutil.ReadAll(res.Body)
+		c.Writer.Write(bytes)
 		c.Abort()
 		return
 		// c.Next()
 	}
 }
 
-// 读写本地转发列表servers.json
-
-func ReadJSONConfig() (map[string]interface{}, error) {
+// ReadJSONConfig 读写本地转发列表servers.json
+func ReadJSONConfig() (*[]Server, error) {
 	file, err := os.Open("servers.json")
 	var servers map[string]interface{}
 	bytes, err := ioutil.ReadAll(file)
+
 	if err != nil {
 		//
 	}
 
 	json.Unmarshal(bytes, &servers)
+	structuredServers := make([]Server, len(servers))
+	for name, s := range servers {
+		server := s.(map[string]interface{})
+		_ = append(structuredServers, Server{name, server["host"].(string), server["port"].(float64)})
+	}
+
+	fmt.Print(structuredServers)
+
 	defer file.Close()
 
-	return servers, nil
-
+	return &structuredServers, nil
 }
